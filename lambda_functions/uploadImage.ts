9db5e90b6b73e0a13ffdfc2c2af118f8a73afdc0
Nelson P Lopes <nelson.npl@gmail.com>
2024-05-03 17:29:59 -0400

    chore: Update .gitignore and package.json

--- lambda_functions/uploadImage.ts
+++ lambda_functions/uploadImage.ts
@@ -1,55 +1,33 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
-import AWS from 'aws-sdk';
+import * as AWS from 'aws-sdk';
+import * as parseMultipart from 'parse-multipart';
+
+const BUCKET = process.env.BUCKET_NAME;
 
 const s3 = new AWS.S3();
 
 export const handler: APIGatewayProxyHandler = async (event, context) => {
-  console.log('event', event);
   try {
-    if (!event.body) {
-      return {
-        statusCode: 400,
-        body: JSON.stringify({ message: 'Corpo da requisição está vazio' }),
-      };
-    }
+    const { filename, data } = extractFile(event);
 
-    const body = JSON.parse(event.body);
-    const imageData = body.imageData;
-
-    const imageBuffer = Buffer.from(imageData, 'base64');
-
-    const imageName = `${Date.now()}.png`;
-    const bucketName = process.env.BUCKET_NAME;
-
-    if (!bucketName) {
-      throw new Error('Variável de ambiente BUCKET_NAME não definida');
-    }
-
-    const params: AWS.S3.PutObjectRequest = {
-      Bucket: bucketName,
-      Key: imageName,
-      Body: imageBuffer,
-      ContentType: 'image/png',
-    };
-
-    const uploadResult = await s3.upload(params).promise();
-
-    const imageUrl = uploadResult.Location;
-
-    console.log('return', {
-      statusCode: 200,
-      body: JSON.stringify({ imageUrl }),
-    });
+    await s3.putObject({ Bucket: BUCKET!, Key: filename, ACL: 'public-read', Body: data }).promise();
 
     return {
       statusCode: 200,
-      body: JSON.stringify({ imageUrl }),
+      body: JSON.stringify({ link: `https://${BUCKET}.s3.amazonaws.com/${filename}` })
     };
-  } catch (error) {
-    console.error('Erro ao fazer upload da imagem:', error);
+  } catch (err) {
     return {
       statusCode: 500,
-      body: JSON.stringify({ message: 'Erro ao fazer upload da imagem' }),
+      body: JSON.stringify({ message: "Erro ao salvar a img" })
     };
   }
 };
+
+function extractFile(event: any) {
+  const boundary = parseMultipart.getBoundary(event.headers['content-type']);
+  const parts = parseMultipart.Parse(Buffer.from(event.body, 'base64'), boundary);
+  const [{ filename, data }] = parts;
+
+  return { filename, data };
+}
